97d50266a4b35ab12f70dc54b38290cdd7350d2d
dennismacc <dennis.maccarthy@gmail.com>
2022-07-28 11:39:16 -0700

    define tables in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'categoryId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'categoryId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'productId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tagId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
 
 module.exports = {
   Product,
