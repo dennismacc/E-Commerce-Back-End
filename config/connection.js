7c7bac9cbd0093cfb7063e66134e48fbef089d2a
dennismacc <dennis.maccarthy@gmail.com>
2022-07-28 15:12:24 -0700

    debugging server connection, connection working ☑️

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: process.env.SQL_PORT || 8889,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
